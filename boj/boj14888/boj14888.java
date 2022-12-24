f8c73a8b2ce45bd2a367091bc54f22092cec18e8
BackJiwan <bn1357nb@gmail.com>
2022-12-24 17:30:10 +0900

    solved boj14888

--- boj/boj14888/boj14888.java
+++ boj/boj14888/boj14888.java
@@ -1,96 +1,75 @@
 package boj.boj14888;
 
+/*
+* 1. 수의 개수 : 최대 11개
+* 2. 수의 최대값 : 100
+* 3. 다루는 최대 크기 : 연산자를 어케 넣어도 항상 -10억보다 크거나 같고 10억보다 작거나 같다는 뜻이다.
+* 4. 최대 크기가 10억을 넘지 않기 때문에 int(-20억 ~ 20억)형을 사용해서 결과를 담을수 있다.
+* 5. 포인트 : 정해진 수열의 사이사이에 연산자를 중복X, 순서O를 지키며 끼워넣는 경우의 수
+* 6. 경우의 수: O(n!/(n-m)!) 이다. -> n개중 m개를 뽑는 순열의 시간 복잡도
+* 7. 수학에서 0!은 1이기 때문에 11! /
+* 문제풀이 : 재귀함수에서는 k를 1번 숫자부터 하나씩 증가시키면서 다음 연산으로 나아간다. 연산의 결과값을 같이 건네준다.
+* 모든 연산자를 다 꺼내서 사용하기 위해서 앞에서부터 하나를 꺼내고 다시 재귀적으로 함수를 호출한다
+* 해당 재귀 함수에서는 이미 사용한 연산자들은 소모된 상태이며 해당 재귀함수도 다시 맨앞의 연산자부터 확인한다.
+* */
+
 import java.io.*;
 import java.util.*;
 
 public class boj14888 {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
+    static int N,max,min;
+    static int[] nums,operands;
 
-    static void input() {
-        N = scan.nextInt();
-        nums = new int[N + 1];
-        operators = new int[5];
-        order = new int[N + 1];
-        for (int i = 1; i <= N; i++) nums[i] = scan.nextInt();
-        for (int i = 1; i <= 4; i++) operators[i] = scan.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         max = Integer.MIN_VALUE;
         min = Integer.MAX_VALUE;
-    }
 
-    static int N, max, min;
-    static int[] nums, operators, order;
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N+1];
+        operands = new int[5];
 
-    // 피연산자 2개와 연산자가 주어졌을 때 계산해주는 함수
-    static int calculator(int operand1, int operator, int operand2){
-        // TODO
-    }
-
-
-    // order[1...N-1] 에 연산자들이 순서대로 저장될 것이다.
-    static void rec_func(int k, int value) {
-        if (k == N) {
-            // 완성된 식에 맞게 계산을 해서 정답에 갱신하는 작업
-            // TODO
-        } else {
-            // k 번째 연산자는 무엇을 선택할 것인가?
-            // TODO
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        for(int i=1;i<N+1;i++){
+            nums[i] = Integer.parseInt(st1.nextToken());
         }
-    }
 
-    public static void main(String[] args) {
-        input();
-        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
-        rec_func(1, nums[1]);
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for(int i=1;i<5;i++){
+            operands[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        rec_func(1,nums[1]);
         sb.append(max).append('\n').append(min);
         System.out.println(sb.toString());
+
     }
 
-
-    static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
+    static int calculator(int a,int b,int operand){
+        if(operand == 1)
+            return a+b;
+        else if(operand == 2)
+            return a-b;
+        else if(operand == 3)
+            return a*b;
+        else
+            return a/b;
+    }
+    static void rec_func(int k,int value){
+        if(k==N){
+            min = Math.min(value,min);
+            max = Math.max(value,max);
+        } else {
+            for(int operator=1;operator<5;operator++){
+                if(operands[operator]>=1){
+                    operands[operator]--;
+                    rec_func(k+1,calculator(value,nums[k+1],operator));
+                    operands[operator]++;
                 }
             }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
         }
     }
+
 }
