f5289d171846cf3920395d44c31015c0ec9c1c9b
BackJiwan <bn1357nb@gmail.com>
2023-02-19 10:43:29 +0900

    solve boj14502

--- boj/boj2667/Main.java
+++ boj/boj2667/Main.java
@@ -7,7 +7,7 @@ package boj.boj2667;
 * 4.DFS가 모두 끝났을때에는 cnt를 ArrayList<Integer>의 새로운 요소로 추가해준다.
 * 5.그리고 cnt는 다시 0이 되고 한번 방문한 노드의 visited 표시는 프로그램 내내 유효하다(static)
 * 6.for(4번)를 이용하여 4개의 동서남북 움직임을 준다.
-* 7.못풀겠어서 강의를 보겠습니다.
+*
 *
 *
 * */
@@ -25,38 +25,31 @@ public class Main {
     static int[][] move = {{1,0},{0,1},{-1,0},{0,-1}};
     static ArrayList<Integer> group;
 
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
     static void input(){
-        N = scan.nextInt();
-        arr = new String[N];
+        N = scan.nextInt(); //격자의 한변 받기
+        arr = new String[N]; //격자 입력 스트링으로
         for(int i=0;i<N;i++){
             arr[i] = scan.nextLine();
         }
-        visited = new boolean[N][N];
-    }
-
-    static void dfs(int x,int y){
-        group_cnt++; //매회탐색마다 카운트를 증가시킨다.
-        visited[x][y] = true;
-        for(int k=0;k<4;k++){
-            int nx = x + move[k][0];
-            int ny = y + move[k][1];
-            if(nx<0||ny<0||nx>=N||ny>=N) continue;
-            if(arr[nx].charAt(ny)=='0') continue;
-            if(visited[nx][ny]) continue;
-            dfs(nx,ny);        }
+        visited = new boolean[N][N]; //방문체크는 N*N
     }
 
     static void pro(){
-        group = new ArrayList<>();
+        group = new ArrayList<>(); //집의 수를 저장하는 어레이리스트(몇개나 나올지 모르기때문에 가변배열 사용)
         for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
+            for(int j=0;j<N;j++){ //격자 완탐
                 if(!visited[i][j] && arr[i].charAt(j)=='1'){
                     //만약 방문한적 없고, 단지가 존재(1이라면)한다면
                     //여기서 부터 연결된 단지의 크기를 카운트하면서 탐색하기 위해서
                     //dfs를 시작한다.
-                    group_cnt =0;
+                    group_cnt =0; //탐색의 시작전에 cnt를 0으로 초기화
                     dfs(i,j);
-                    group.add(group_cnt);
+                    group.add(group_cnt); //모든 탐색이 끝나면 cnt값을 가변리스트에 추가
                 }
             }
         }
@@ -66,9 +59,16 @@ public class Main {
         System.out.println(sb.toString());
     }
 
-    public static void main(String[] args) {
-        input();
-        pro();
+    static void dfs(int x,int y){
+        group_cnt++; //매회탐색마다 카운트를 증가시킨다. 탐색 = dfs호출
+        visited[x][y] = true;
+        for(int k=0;k<4;k++){
+            int nx = x + move[k][0];
+            int ny = y + move[k][1]; //x와y의 다음 위치
+            if(nx<0||ny<0||nx>=N||ny>=N) continue; //불가능한 위치라면 반려
+            if(arr[nx].charAt(ny)=='0') continue; //위치값이 0이라면 반려
+            if(visited[nx][ny]) continue; //방문했다면 반려
+            dfs(nx,ny);        } //재귀 dfs호출
     }
 
     static class FastReader {
