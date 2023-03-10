711baae2eeacab56a51ecf3493777372ed004efe
BackJiwan <bn1357nb@gmail.com>
2023-03-10 21:10:40 +0900

    solve boj2178 in java

--- boj/boj2178/Main.java
+++ boj/boj2178/Main.java
@@ -1,111 +1,111 @@
-package boj.boj2178;
-
-/*
-* 1.시작 위치는 (1,1) 도착 위치는 (N,M) 고정
-* 2.배열의 크기가 최대일때에는 N * M = 10,000 = 1만
-* 3.칸을 저장하는 N크기의 String을 저장하는 배열 map
-* 4.시작점에서부터 BFS탐색을 시작한다.
-* */
-
-import java.util.*;
-import java.io.*;
-
-public class Main {
-    static FastReader scan = new FastReader();
-    static StringBuilder sb = new StringBuilder();
-
-    static int N,M,ans;
-    static String[] map;
-    static boolean[][] visit;
-    static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
-    static ArrayList<Integer>[] adj;
-    static int[][] dist;
-
-    public static void main(String[] args){
-        input();
-        pro();
-    }
-
-    static void input(){
-        N = scan.nextInt();
-        M = scan.nextInt();
-        map = new String[N];
-        for(int i=0;i<N;i++){
-            map[i] = scan.nextLine();
-        }
-        visit = new boolean[N][M];
-        dist = new int[N][M];
-    }
-
-    static void pro(){
-        bfs(0,0); //시작점은 0,0이다.
-        System.out.println(dist[N-1][M-1]); // 도착점까지의 최소 이동 간선수 출력
-    }
-
-    static void bfs(int x,int y){
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                dist[i][j] = -1; //dist 배열을 -1로 초기화
-            }
-        }
-        Queue<Integer> que = new LinkedList<>();
-        que.add(x);
-        que.add(y); //순서대로 두쌍씩 꺼내기만 하면 x와 y를 하나씩 저장해도 괜찮다.
-        dist[x][y] = 1; //일단 시작점부터 1을 카운트한다.
-        visit[x][y] = true; //방문체크
-
-        while(!que.isEmpty()){ //bfs시작
-            x = que.poll();
-            y = que.poll();
-            for(int k=0;k<4;k++){
-                int nx = x + dir[k][0];
-                int ny = y + dir[k][1]; //다음 x,y 좌표를 동서남북으로 한번씩 대입한다.
-                if(nx<0||ny<0||nx>=N|ny>=M) continue; //지도를 벗어나는지
-                if(map[nx].charAt(ny) =='0') continue; //방문 불가능한 정점인지
-                if(visit[nx][ny]) continue; //방문한적이 있는지
-                que.add(nx); //조건을 만족하는 방문한적 없는 정점이라면 큐에 추가한다.
-                que.add(ny);
-                visit[nx][ny] = true;
-                dist[nx][ny] = dist[x][y]+1; //다음칸으로 방문하면서 기존보다 거리는 1 늘어난것을 저장해준다.
-            }
-        }
-    }
-
-
-    static class FastReader{
-        BufferedReader br;
-        StringTokenizer st;
-        public FastReader(){
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-        public FastReader(String s) throws FileNotFoundException{
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-        String next(){
-            while(st==null || !st.hasMoreElements()){
-                try{
-                    st = new StringTokenizer(br.readLine());
-                }catch(IOException e){
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-        int nextInt(){
-            return Integer.parseInt(next());
-        }
-        long nextLong(){
-            return Long.parseLong(next());
-        }
-        String nextLine(){
-            String str = "";
-            try{
-                str = br.readLine();
-            }catch(IOException e){
-                e.printStackTrace();
-            }
-            return str;
-        }
-
-    }
-}
+//package boj.boj2178;
+//
+///*
+//* 1.시작 위치는 (1,1) 도착 위치는 (N,M) 고정
+//* 2.배열의 크기가 최대일때에는 N * M = 10,000 = 1만
+//* 3.칸을 저장하는 N크기의 String을 저장하는 배열 map
+//* 4.시작점에서부터 BFS탐색을 시작한다.
+//* */
+//
+//import java.util.*;
+//import java.io.*;
+//
+//public class Main {
+//    static FastReader scan = new FastReader();
+//    static StringBuilder sb = new StringBuilder();
+//
+//    static int N,M,ans;
+//    static String[] map;
+//    static boolean[][] visit;
+//    static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+//    static ArrayList<Integer>[] adj;
+//    static int[][] dist;
+//
+//    public static void main(String[] args){
+//        input();
+//        pro();
+//    }
+//
+//    static void input(){
+//        N = scan.nextInt();
+//        M = scan.nextInt();
+//        map = new String[N];
+//        for(int i=0;i<N;i++){
+//            map[i] = scan.nextLine();
+//        }
+//        visit = new boolean[N][M];
+//        dist = new int[N][M];
+//    }
+//
+//    static void pro(){
+//        bfs(0,0); //시작점은 0,0이다.
+//        System.out.println(dist[N-1][M-1]); // 도착점까지의 최소 이동 간선수 출력
+//    }
+//
+//    static void bfs(int x,int y){
+//        for(int i=0;i<N;i++){
+//            for(int j=0;j<M;j++){
+//                dist[i][j] = -1; //dist 배열을 -1로 초기화
+//            }
+//        }
+//        Queue<Integer> que = new LinkedList<>();
+//        que.add(x);
+//        que.add(y); //순서대로 두쌍씩 꺼내기만 하면 x와 y를 하나씩 저장해도 괜찮다.
+//        dist[x][y] = 1; //일단 시작점부터 1을 카운트한다.
+//        visit[x][y] = true; //방문체크
+//
+//        while(!que.isEmpty()){ //bfs시작
+//            x = que.poll();
+//            y = que.poll();
+//            for(int k=0;k<4;k++){
+//                int nx = x + dir[k][0];
+//                int ny = y + dir[k][1]; //다음 x,y 좌표를 동서남북으로 한번씩 대입한다.
+//                if(nx<0||ny<0||nx>=N|ny>=M) continue; //지도를 벗어나는지
+//                if(map[nx].charAt(ny) =='0') continue; //방문 불가능한 정점인지
+//                if(visit[nx][ny]) continue; //방문한적이 있는지
+//                que.add(nx); //조건을 만족하는 방문한적 없는 정점이라면 큐에 추가한다.
+//                que.add(ny);
+//                visit[nx][ny] = true;
+//                dist[nx][ny] = dist[x][y]+1; //다음칸으로 방문하면서 기존보다 거리는 1 늘어난것을 저장해준다.
+//            }
+//        }
+//    }
+//
+//
+//    static class FastReader{
+//        BufferedReader br;
+//        StringTokenizer st;
+//        public FastReader(){
+//            br = new BufferedReader(new InputStreamReader(System.in));
+//        }
+//        public FastReader(String s) throws FileNotFoundException{
+//            br = new BufferedReader(new FileReader(new File(s)));
+//        }
+//        String next(){
+//            while(st==null || !st.hasMoreElements()){
+//                try{
+//                    st = new StringTokenizer(br.readLine());
+//                }catch(IOException e){
+//                    e.printStackTrace();
+//                }
+//            }
+//            return st.nextToken();
+//        }
+//        int nextInt(){
+//            return Integer.parseInt(next());
+//        }
+//        long nextLong(){
+//            return Long.parseLong(next());
+//        }
+//        String nextLine(){
+//            String str = "";
+//            try{
+//                str = br.readLine();
+//            }catch(IOException e){
+//                e.printStackTrace();
+//            }
+//            return str;
+//        }
+//
+//    }
+//}
---
+++ boj/boj2178/Main2.java
@@ -0,0 +1,114 @@
+package boj.boj2178;
+
+/*
+* 1.N,M 입력받기
+* 2.bfs로 탐색을 (0,0) 부터 시작
+* 3.순서대로 x,y를 넣어주고 쌍으로 꺼내주면 Integer Queue 사용해도 가능
+* 4.범위내,방문하지않은,0이 아닌 경우 라면 다음 정점(동,서,남,북)으로 이동 (nx,ny) 하면서
+*   dist[nx][ny] = dist[x][y] + 1 를 통해서 간선의 수를 카운트한다.
+* */
+
+import java.util.*;
+import java.io.*;
+
+public class Main2 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N,M;
+    static String[] map;
+    static boolean[][] visited;
+    static int[][] dist;
+    static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+
+    public static void main(String[] args){
+        input();
+        pro();
+    }
+
+    static void input(){
+        N = scan.nextInt();
+        M = scan.nextInt();
+        map = new String[N];
+        for(int i=0;i<N;i++){ //map은 chatAt으로 y좌표에 접근한다.
+            map[i] = scan.nextLine();
+        }
+        visited = new boolean[N][M];
+        dist = new int[N][M];
+    }
+
+    static void pro(){
+        //시작점은 0,0이다.
+        bfs(0,0);
+        //도착점에서의 dist에 담긴 값이 최단 이동횟수이다.
+        System.out.println(dist[N-1][M-1]);
+    }
+
+    static void bfs(int x,int y) {
+        //dist -1로 초기화
+        for(int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                dist[i][j] = -1;
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>(); //시작위치를 큐에 넣는다.
+        queue.add(x);
+        queue.add(y);
+        dist[x][y] = 1; //시작점의 dist는 1부터 시작한다. //행위마다 dist와 visited를 조작해야 한다.
+        visited[x][y] = true;
+
+        //BFS
+        while(!queue.isEmpty()){
+            x = queue.poll();
+            y = queue.poll();
+            for(int k=0;k<4;k++){
+                int nx = x + dir[k][0];
+                int ny = y + dir[k][1];
+                if(nx<0 || ny<0 || nx>=N || ny>=M) continue; //맵을 벗어난다면
+                if(map[nx].charAt(ny) =='0') continue; //갈수 없는 칸이면
+                if(visited[nx][ny]) continue; //이미 방문했다면
+                queue.add(nx);
+                queue.add(ny);
+                visited[nx][ny] = true;
+                dist[nx][ny] = dist[x][y] + 1;
+            }
+        }
+
+    }
+
+    static class FastReader{
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader(){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        public FastReader(String s) throws FileNotFoundException{
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+        String next(){
+            while(st==null || !st.hasMoreElements()){
+                try{
+                    st = new StringTokenizer(br.readLine());
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt(){
+            return Integer.parseInt(next());
+        }
+        long nextLong(){
+            return Long.parseLong(next());
+        }
+        String nextLine(){
+            String str = "";
+            try{
+                str = br.readLine();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
