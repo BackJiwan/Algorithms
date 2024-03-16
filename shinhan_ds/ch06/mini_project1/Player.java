d0f60ad11ee2cb16308beeb2053e03318f9c6d58
BackJiwan <bn1357nb@gmail.com>
2024-03-16 20:28:47 +0900

    feat: Minimum features for Omok - 오목 게임의 최초 커밋이며 19*19칸의 오목판을 출력하는 기능만 존재

---
+++ shinhan_ds/ch06/mini_project1/Player.java
@@ -0,0 +1,10 @@
+package shinhan_ds.ch06.mini_project1;
+
+public class Player {
+    String name;
+    String stone;
+    Player(String name, String stone) {
+        this.name = name;
+        this.stone = stone;
+    }
+}
